449bc2fa38c7963166441c6c16999cff2addc6bd
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2026-03-11 09:39:52 +0530

    create context for token manager

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,24 @@ const API = axios.create({
 
 });
 
+// logout
+export const logoutUser = async () => {
+    try {
+        const response = await API.post(`/auths/logout`,
+            {},
+            { withCredentials: true }
+        );
+        return response.data;
+    } catch (err) {
+        console.error('Logout error:', {
+            message: err.message,
+            status: err.response?.status,
+            data: err.response?.data,
+        });
+        throw err;
+    }
+};
+
 // Get all travel events
 export const getAllEvents = async () => {
     try {
