b7cc8a5cd5efd3ee5013cf7414d0bca2fbaa1405
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2026-03-14 11:19:45 +0530

    Fix Vercel server routing

--- backend/server.js
+++ backend/server.js
@@ -6,27 +6,13 @@ import authRouter from "./routes/auth.route.js";
 import travelEventRouter from "./routes/travelEvent.routes.js";
 
 const app = express();
-
-import cors from "cors";
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors());
+app.options("*", cors());
 
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "https://mini-travel-experience-listing-platform-x881-n8nikz66a.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 
 app.use(
   "/item-images",
